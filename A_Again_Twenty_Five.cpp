bd680b501408c97617b4eb196685a0eedb1cd4c0
iamAditya-Pandey <pandeyaditya19012006@gmail.com>
2026-01-04 20:02:42 +0530

    Add files via upload

---
+++ A_Again_Twenty_Five.cpp
@@ -0,0 +1,31 @@
+// JAI SHREE RAM //
+/*******************************************************
+ * Author: Aditya Pandey
+ * Purpose: Competitive Programming Template
+ * Fast I/O + Optimized compilation flags
+ *******************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+#define endl '\n'
+#define int long long  // Use 64-bit integers by default
+#define all(x) (x).begin(), (x).end()
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+// Debug macro (optional, use only in local testing)
+#ifndef ONLINE_JUDGE
+    #define debug(x) cerr << #x << " = " << x << endl;
+#else
+    #define debug(x)
+#endif
+
+int32_t main() {
+    fast_io;
+
+    cout<<25<<endl;
+
+    return 0;
+}
