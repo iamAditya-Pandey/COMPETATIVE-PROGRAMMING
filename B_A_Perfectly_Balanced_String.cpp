b84765086434abcd954415dfbde7c93c64c4e7f4
iamAditya-Pandey <pandeyaditya19012006@gmail.com>
2026-01-05 14:31:19 +0530

    Add files via upload

---
+++ B_A_Perfectly_Balanced_String.cpp
@@ -0,0 +1,88 @@
+// JAI SHREE RAM //
+/*******************************************************
+ * Author: Aditya Pandey
+ * Purpose: Competitive Programming Template
+ * Fast I/O + Optimized compilation flags
+ *******************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+#define endl '\n'
+#define int long long  // Use 64-bit integers by default
+
+// Common macros
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define sz(x) ((int)(x).size())
+
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+// Sorting macros
+#define sorta(x) sort(all(x))
+#define sortd(x) sort((x).rbegin(), (x).rend())
+
+// Loops
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rep1(i, n) for (int i = 1; i <= (n); i++)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; i--)
+#define rrep1(i, n) for (int i = (n); i >= 1; i--)
+
+// Vector shortcuts (int type)
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define pii pair<int,int>
+#define vpi vector<pair<int,int>>
+#define vvpi vector<vector<pair<int,int>>>
+
+// Debug macro
+#ifndef ONLINE_JUDGE
+    #define debug(x) cerr << #x << " = " << x << endl;
+#else
+    #define debug(x)
+#endif
+
+int32_t main() {
+    fast_io;
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        // Write your test case logic here
+        string s;
+	cin >> s; 
+	int n = s.size();
+	set<char> c; 
+	int k;
+    bool flag = true;
+	
+	for (k=0;k<n;k++){ 
+		if (c.find(s[k]) == c.end()){
+			c.insert(s[k]); 
+		} else{
+			break;
+		}
+	}
+	for (int i=k;i<n;i++){
+		if (s[i] != s[i-k]){
+			flag =false;
+			break;
+		}
+	}
+	if(flag){
+        cout<<"YES"<<endl; 
+    }
+    else{
+        cout<<"NO"<<endl; 
+    }
+
+    }
+
+    return 0;
+}
