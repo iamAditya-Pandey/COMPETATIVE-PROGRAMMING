a4d44b46b68820ff2403d657e095285943c2a28d
iamAditya-Pandey <pandeyaditya19012006@gmail.com>
2026-01-10 22:24:32 +0530

    Add files via upload

---
+++ A_Fibonacciness.cpp
@@ -0,0 +1,75 @@
+// JAI SHREE RAM //
+/*******************************************************
+ * Author: Aditya Pandey
+ * Purpose: Competitive Programming Template
+ * Fast I/O + Optimized compilation flags
+ *******************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+#define endl '\n'
+#define int long long  // Use 64-bit integers by default
+
+// Common macros
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define sz(x) ((int)(x).size())
+
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+// Sorting macros
+#define sorta(x) sort(all(x))
+#define sortd(x) sort((x).rbegin(), (x).rend())
+
+// Loops
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rep1(i, n) for (int i = 1; i <= (n); i++)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; i--)
+#define rrep1(i, n) for (int i = (n); i >= 1; i--)
+
+// Vector shortcuts (int type)
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define pii pair<int,int>
+#define vpi vector<pair<int,int>>
+#define vvpi vector<vector<pair<int,int>>>
+
+// Debug macro
+#ifndef ONLINE_JUDGE
+    #define debug(x) cerr << #x << " = " << x << endl;
+#else
+    #define debug(x)
+#endif
+
+int32_t main() {
+    fast_io;
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        // Write your test case logic here
+         int a1,a2,a4,a5;
+        cin>>a1>>a2>>a4>>a5;
+        int bt=0;
+        for (int a3=-100;a3<=200;a3++){
+            int fibo=0;
+            int arr[5] = {a1,a2,a3,a4,a5};
+            for (int i=0;i<3;i++){
+                if (arr[i]+arr[i+1]==arr[i+2]){
+                    fibo++;
+                }
+            }
+            bt = max(bt,fibo);
+        }
+        cout<<bt<<endl;
+    }
+
+    return 0;
+}
