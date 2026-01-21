063c87b55d7bf0962cf40e042ae221492c1314af
iamAditya-Pandey <pandeyaditya19012006@gmail.com>
2026-01-21 17:43:10 +0530

    Add files via upload

---
+++ A_Medium_Number.cpp
@@ -0,0 +1,44 @@
+// JAI SHREE RAM //
+/*******************************************************
+ * Author: Aditya Pandey
+ * Purpose: Competitive Programming Template
+ * Fast I/O + Optimized compilation flags
+ *******************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+#define endl '\n'
+#define int long long  // Use 64-bit integers by default
+#define all(x) (x).begin(), (x).end()
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+// Debug macro (optional, use only in local testing)
+#ifndef ONLINE_JUDGE
+    #define debug(x) cerr << #x << " = " << x << endl;
+#else
+    #define debug(x)
+#endif
+
+int32_t main() {
+    fast_io;
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        // Write your test case logic here
+
+        vector<int>v(3);
+        for(int i=0;i<3;i++){
+                cin>>v[i];
+        }
+        sort(all(v));
+
+        cout<<v[1]<<endl;
+    }
+
+    return 0;
+}
