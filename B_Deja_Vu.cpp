025a55d5e21d6d78684f7517323e2014d8503011
iamAditya-Pandey <pandeyaditya19012006@gmail.com>
2026-02-04 01:02:03 +0530

    Add files via upload

---
+++ B_Deja_Vu.cpp
@@ -0,0 +1,93 @@
+// JAI SHREE RAM //
+/*******************************************************
+ * Author: Aditya Pandey
+ * Purpose: Competitive Programming Template
+ * Fast I/O + Optimized compilation flags
+ *******************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+#define endl '\n'
+#define int long long  // Use 64-bit integers by default
+
+// Common macros
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define sz(x) ((int)(x).size())
+
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+// Sorting macros
+#define sorta(x) sort(all(x))
+#define sortd(x) sort((x).rbegin(), (x).rend())
+
+// Loops
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rep1(i, n) for (int i = 1; i <= (n); i++)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; i--)
+#define rrep1(i, n) for (int i = (n); i >= 1; i--)
+
+// Vector shortcuts (int type)
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define pii pair<int,int>
+#define vpi vector<pair<int,int>>
+#define vvpi vector<vector<pair<int,int>>>
+
+// Debug macro
+#ifndef ONLINE_JUDGE
+    #define debug(x) cerr << #x << " = " << x << endl;
+#else
+    #define debug(x)
+#endif
+
+int32_t main() {
+    fast_io;
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        // Write your test case logic here
+
+        int n,q;
+        cin>>n>>q;
+
+        vi a(n);
+        rep(i,n){
+            cin>>a[i];
+        }
+
+        vi x(q);
+        rep(i,q){
+            cin>>x[i];
+        }
+
+        int pre=31;
+
+        for(int i=0;i<q;i++){
+            if(x[i]>=pre)continue;
+
+            int val =pow(2,x[i]);
+
+            for(int j=0;j<n;j++){
+                if(a[j]%val==0){
+                    a[j]+=(val/2);
+                }
+            }
+            pre=x[i];
+        }
+        for(int i=0;i<n;i++){
+            cout<<a[i]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
