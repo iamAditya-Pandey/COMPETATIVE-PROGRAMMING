9062cd90e7aa63fa53b213112302de2d27b1ba87
iamAditya-Pandey <pandeyaditya19012006@gmail.com>
2026-02-10 21:58:21 +0530

    Add files via upload

---
+++ E_1_Rudolf_and_Snowflakes_simple_version.cpp
@@ -0,0 +1,79 @@
+// JAI SHREE RAM //
+/*******************************************************
+ * Author: Aditya Pandey
+ * Purpose: Competitive Programming Template
+ * Fast I/O + Optimized compilation flags
+ *******************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+#define endl '\n'
+#define int long long  // Use 64-bit integers by default
+
+// Common macros
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define sz(x) ((int)(x).size())
+
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+// Sorting macros
+#define sorta(x) sort(all(x))
+#define sortd(x) sort((x).rbegin(), (x).rend())
+
+// Loops
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rep1(i, n) for (int i = 1; i <= (n); i++)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; i--)
+#define rrep1(i, n) for (int i = (n); i >= 1; i--)
+
+// Vector shortcuts (int type)
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define pii pair<int,int>
+#define vpi vector<pair<int,int>>
+#define vvpi vector<vector<pair<int,int>>>
+
+// Debug macro
+#ifndef ONLINE_JUDGE
+    #define debug(x) cerr << #x << " = " << x << endl;
+#else
+    #define debug(x)
+#endif
+
+int32_t main() {
+    fast_io;
+
+    int t;
+    cin>>t;
+
+    vi isposs(1000001,0);
+
+    for (int i=2;i<=10000;i++){
+        int cur = i*i*i;
+        int cur1=(cur-1)/(i-1);
+
+        while(cur1<=1000000){
+            isposs[cur1] = 1;
+            cur*= i;
+            cur1 = (cur-1)/(i-1);
+        }
+    }
+
+    while (t--) {
+        int n;
+        cin >> n;
+        if(isposs[n]){
+            yes;
+        }
+        else{
+            no;
+        }
+    }
+}
