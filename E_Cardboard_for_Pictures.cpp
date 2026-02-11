92ae7d46ac46d95475c1df3b9d76490abe9253b2
iamAditya-Pandey <pandeyaditya19012006@gmail.com>
2026-02-11 19:40:44 +0530

    Add files via upload

---
+++ E_Cardboard_for_Pictures.cpp
@@ -0,0 +1,96 @@
+// JAI SHREE RAM //
+/*******************************************************
+ * Author: Aditya Pandey
+ * Purpose: Competitive Programming Template
+ * Fast I/O + Optimized compilation flags
+ *******************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+#define endl '\n'
+#define int long long  // Use 64-bit integers by default
+
+// Common macros
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define sz(x) ((int)(x).size())
+
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+// Sorting macros
+#define sorta(x) sort(all(x))
+#define sortd(x) sort((x).rbegin(), (x).rend())
+
+// Loops
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rep1(i, n) for (int i = 1; i <= (n); i++)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; i--)
+#define rrep1(i, n) for (int i = (n); i >= 1; i--)
+
+// Vector shortcuts (int type)
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define pii pair<int,int>
+#define vpi vector<pair<int,int>>
+#define vvpi vector<vector<pair<int,int>>>
+
+// Debug macro
+#ifndef ONLINE_JUDGE
+    #define debug(x) cerr << #x << " = " << x << endl;
+#else
+    #define debug(x)
+#endif
+
+int32_t main() {
+    fast_io;
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        // Write your test case logic here
+
+        int n,c;
+        cin>>n>>c;
+
+        vi a(n);
+        rep(i,n){
+            cin>>a[i];
+        }
+
+        int l=0;
+        int r = 1e9;
+         int ans =-1;
+
+        while(l<=r){
+
+            int mid = l+(r-l)/2;
+            int sum =0;
+
+            for (int i=0;i<n;++i){
+			sum +=(a[i] + 2*mid)*(a[i] + 2*mid);
+			if(sum>c) 
+            break; 
+		}
+
+        if (sum<=c){
+			ans = mid;
+			l = mid+1; 
+		} 
+        
+        else {
+			r = mid-1;
+		}
+        }
+
+        cout<<ans<<endl;
+    }
+
+    return 0;
+}
