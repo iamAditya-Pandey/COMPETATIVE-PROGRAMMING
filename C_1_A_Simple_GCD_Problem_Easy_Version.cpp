1d7a5de91ccd62445f1643ec82b1473d0ec5da01
Aditya Pandey <pandeyaditya19012006@gmail.com>
2026-03-30 18:57:01 +0530

    Add files via upload

---
+++ C_1_A_Simple_GCD_Problem_Easy_Version.cpp
@@ -0,0 +1,84 @@
+// JAI SHREE RAM //
+/*******************************************************
+ * Author: Aditya Pandey
+ * Purpose: Competitive Programming Template
+ * Fast I/O + Optimized compilation flags
+ *******************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+#define endl '\n'
+#define int long long  // Use 64-bit integers by default
+
+// Common macros
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define sz(x) ((int)(x).size())
+
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+// Sorting macros
+#define sorta(x) sort(all(x))
+#define sortd(x) sort((x).rbegin(), (x).rend())
+
+// Loops
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rep1(i, n) for (int i = 1; i <= (n); i++)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; i--)
+#define rrep1(i, n) for (int i = (n); i >= 1; i--)
+
+// Vector shortcuts (int type)
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define pii pair<int,int>
+#define vpi vector<pair<int,int>>
+#define vvpi vector<vector<pair<int,int>>>
+
+// Debug macro
+#ifndef ONLINE_JUDGE
+    #define debug(x) cerr << #x << " = " << x << endl;
+#else
+    #define debug(x)
+#endif
+
+int lcm(int x,int y){
+    return (x/__gcd(x,y))*y;
+}
+
+int32_t main() {
+    fast_io;
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        // Write your test case logic here
+    int n;
+    cin>>n;
+    
+    vi a(n);
+    rep(i,n) cin>>a[i];
+    
+    vi b(n);
+    rep(i,n) cin>>b[i]; 
+
+    int ans=0;
+
+    for(int i=1;i<n-1;i++){
+        if(lcm(a[i-1],a[i+1])%a[i]!=0) ans++;
+    }
+
+	if(a[1]%a[0]!=0) ans++;
+    if(a[n-2]%a[n-1]!=0) ans++;
+
+    cout<<ans<<endl;
+    }
+
+    return 0;
+}
