182198d2c15c2cf087ba35b03e2dcf58a36303a3
Aditya Pandey <pandeyaditya19012006@gmail.com>
2026-04-04 14:45:14 +0530

    Add files via upload

---
+++ B_Filya_and_Homework.cpp
@@ -0,0 +1,75 @@
+// JAI SHREE RAM //
+/*******************************************************
+ * Author: Aditya Pandey
+ * Purpose: Competitive Programming Template
+ * Fast I/O + Optimized compilation flags
+ *******************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+#define endl '\n'
+#define int long long  // Use 64-bit integers by default
+
+// Common macros
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define sz(x) ((int)(x).size())
+
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+// Sorting macros
+#define sorta(x) sort(all(x))
+#define sortd(x) sort((x).rbegin(), (x).rend())
+
+// Loops
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rep1(i, n) for (int i = 1; i <= (n); i++)
+#define rrep(i, n) for (int i = (n)-1; i >= 0; i--)
+#define rrep1(i, n) for (int i = (n); i >= 1; i--)
+
+// Vector shortcuts (int type)
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define pii pair<int,int>
+#define vpi vector<pair<int,int>>
+#define vvpi vector<vector<pair<int,int>>>
+
+// Debug macro
+#ifndef ONLINE_JUDGE
+    #define debug(x) cerr << #x << " = " << x << endl;
+#else
+    #define debug(x)
+#endif
+
+int32_t main() {
+    fast_io;
+
+    int n;
+    cin>>n;
+
+    vi a(n);
+    set<int> st; 
+    rep(i,n){
+        cin>>a[i];
+        st.insert(a[i]);
+    }
+
+    if(st.size()==1 || st.size()==2){
+        yes;
+        return 0;
+    }
+      if(st.size()==3){
+        vi v(st.begin(),st.end());
+        if(v[1]-v[0] == v[2]-v[1]) yes;
+        else no;
+        return 0;
+    }
+    no;
+    return 0;
+}
